d0075ce2aafb8c57b210701c08e2294e85c46072
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2023-07-26 16:51:03 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,50 +1,53 @@
-#include <iostream>
-#include <chrono>
-#include <bitset>
-#include <cmath>
+#include <bits/stdc++.h>
+void sieve_bool(size_t n);
+void sieve_char(size_t n);
+
+template<typename Function>
+double benchmark(Function&& func, int argument, int num_iterations) {
+    auto start_time = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < num_iterations; ++i) {
+        func(argument);
+    }
+    auto end_time = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration<double, std::milli>(end_time - start_time).count() / num_iterations;
+}
+
 int main() {
-	//find primes until the range (2 <= x < range)
-	constexpr auto range = 1'000'000;
-	
-	auto start = std::chrono::steady_clock::now();
-	std::bitset<range>* composite = new std::bitset<range>;
-	
-	//mark all even composite
-	for (int i{ 4 }; i < range; i += 2) {
-		(*composite)[i] = 1;
-	}
-	
-	//check for odd numbers
-	for (int i{ 3 }; i < std::sqrt(range); i += 2) {
-		if (!(*composite)[i]) {
-			for (int j{ i * i }; j < range; j += 2 * i) { 
-				(*composite)[j] = 1; 
-			}
-		}
-	}
+    std::cout << "Finding primes (1 Billion range)" << '\n';
+    double t = benchmark(sieve_bool, 1'000'000'000, 1);
+    std::cout << "Average using C++ std::vector<bool>: " << t/1000 << " seconds\n";
+    double v = benchmark(sieve_char, 1'000'000'000, 1);
+    std::cout << "Average using C char* + malloc: " << v/1000 << " seconds";
+}
 
-	std::cout << "Bytes occupied: " << sizeof * composite << '\n';
-	auto end = std::chrono::steady_clock::now();
-	auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
-	
-	std::cout << "Duration: " << elapsed_time << 's' << '\n';
-	composite->flip(); 
-	size_t num_primes = composite->count() - 2;
-	std::cout << "Primes: " << num_primes << '\n' << '\n';
-	composite->flip();
-	int show_primes{};
-	std::cout << "Show prime numbers? (1/0) >>";
-	std::cin >> show_primes;
+void sieve_bool(size_t n) {
+    static std::vector<bool> composite_vec(n, false);
+    for (size_t i {4}; i < n; i += 2) {
+        composite_vec[i] = true;
+    }
+    const auto sqrtn = static_cast<size_t>(std::sqrt(n));
+    for (size_t i {3}; i < sqrtn; ++i) {
+        if (!composite_vec[i]) {
+            for (size_t j { i * i }; j < n; j += i << 1) {
+                composite_vec[i] = true;
+            }
+        }
+    }
+}
 
-	if (show_primes) {
-		for (int i{ 2 }; i < range; ++i) {
-			if (!(*composite)[i]) {
-				std::cout << i << ' ';
-			}
-		}
-	}
-	
-	system("pause");
-	delete composite;
-	return 0;
+void sieve_char(size_t n) {
+    char* composite_list = (char*) malloc(n);
+    memset(composite_list, 0, n);
+    for (size_t i {4}; i < n; i += 2) {
+        composite_list[i] = 1;
+    }
+    auto sqrtn = static_cast<size_t>(std::sqrt(n));
+    for (size_t i {3}; i < sqrtn; ++i) {
+        if (!composite_list[i]) {
+            for (size_t j { i * i }; j < n; j += i << 1) {
+                composite_list[j] = 1;
+            }
+        }
+    }
+    free(composite_list);
 }
